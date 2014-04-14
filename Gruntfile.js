8d4312b45245871031ae08e2baa1d1ea0e990057
Rick Eyre <rick.eyre@hotmail.com>
2014-04-14 13:43:42 -0400

    Add Gruntfile with bump and jshint.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+// The MIT License (MIT)
+// Copyright (c) little-helper contributors.
+
+module.exports = function( grunt ) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON( "package.json" ),
+
+    jshint: {
+      options: {
+        esnext: true,
+        indent: 2,
+        expr: true,
+        camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        newcap: true,
+        unused: true,
+        trailing: true,
+        browser: true,
+        node: true
+      },
+      files: [
+        "lib/*",
+        "bin/*"
+      ]
+    },
+
+    bump: {
+      options: {
+        files: [ "package.json" ],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: "Release v%VERSION%",
+        commitFiles: [ "package.json" ],
+        createTag: true,
+        tagName: "v%VERSION%",
+        tagMessage: "Version %VERSION%",
+        push: true,
+        pushTo: "git@github.com:RickEyre/little-helper.git",
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks( "grunt-contrib-jshint" );
+  grunt.loadNpmTasks( "grunt-bump" );
+
+  grunt.registerTask( "default", [ "jshint" ]);
+};
